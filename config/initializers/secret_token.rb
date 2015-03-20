53f5c755535950a6f27dfa7f9c365130a87b4467
Travis Olbrich <travis.olbrich@gmail.com>
2015-03-20 15:21:14 -0500

    Initial Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MhmrTroubleshooter::Application.config.secret_token = '0914150874f022bca64ffbc0932abe8b52fa8207e65b91628afe6764c4c1a694dff5a13c40587b4c079bc120c6e3bdf7335e8b3688a8f1ed0656fed3dfbfd60e'
