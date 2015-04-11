1ccf613d73af447f6e849743c40c38f4f07059ab
bote795 <nick_n_b@yahoo.com>
2015-04-11 17:44:42 -0400

    edited application.rb bk to false for when pushing to production

--- config/application.rb
+++ config/application.rb
@@ -47,8 +47,8 @@ module MhmrTroubleshooter
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
-    #change this when pushing to productiomn
-    config.assets.initialize_on_precompile = true
+    #change this to false when pushing to productiomn
+    config.assets.initialize_on_precompile = false
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
