442c6cba9528ec1fbd8ebce8854d4cb69aef0ec4
bote795 <nick_n_b@yahoo.com>
2015-04-14 12:21:35 -0400

    fixed email cucumber

--- features/step_definitions/email_steps.rb
+++ features/step_definitions/email_steps.rb
@@ -3,13 +3,16 @@ Given /^I have no emails$/ do
 end
 Given /^I have emails name (.+)$/ do |names|
 	names.split(',').each do |name|
-		Email.create!(:name => name)
+		Email.create!(:email_address => name)
 	end
 end
 Given /^I have a email name (.+)$/ do |name|
-     @email= Email.create!(:name => name)
+     @email= Email.create!(:email_address => name)
 end
 Then /^I should have ([0-9]+) emails$/ do |count|
+    Email.count.should == count.to_i
+end
+Then /^I should have ([0-9]+) email$/ do |count|
     Email.count.should == count.to_i
 end
 
