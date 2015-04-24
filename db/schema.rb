021efe6368a5e458e576a76570abf89211e5750f
bote795 <nick_n_b@yahoo.com>
2015-04-24 01:20:18 -0400

    working on categoriesUI

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150408041838) do
+ActiveRecord::Schema.define(:version => 20150421181248) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -34,15 +34,36 @@ ActiveRecord::Schema.define(:version => 20150408041838) do
   end
 
   create_table "tickets", :force => true do |t|
-    t.string  "requestor"
-    t.string  "phone"
-    t.integer "ru"
-    t.string  "email"
-    t.string  "room"
-    t.string  "subject"
-    t.text    "description"
-    t.integer "location_id"
-    t.integer "ticket_category_id"
+    t.string   "requestor"
+    t.string   "phone"
+    t.integer  "ru"
+    t.string   "email"
+    t.string   "room"
+    t.string   "subject"
+    t.text     "description"
+    t.integer  "location_id"
+    t.integer  "ticket_category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0,  :null => false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+    t.boolean  "admin"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
