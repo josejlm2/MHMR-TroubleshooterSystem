e664e49854d47a4df2eff31b0c103bae65adbad9
bote795 <nick_n_b@yahoo.com>
2015-05-01 17:22:04 -0400

    added the date in summary page and added actually only looking by that date. Included methods for specific month summary

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,8 @@
 class Ticket < ActiveRecord::Base
- scope :created_in_month, ->(month) { where("DATE_PART('month', created_at) = ?", month) }
- scope :closed_in_month, ->(month) { where("DATE_PART('month',  updated_at) = ? AND status_id = ?", month, Status.find_by_name("Completed").id) }
+ scope :created_in_month, ->(month) { where("DATE_PART('month', created_at) = ?  AND DATE_PART('year',  updated_at) = ?", month, Date.today.year) }
+ scope :closed_in_month, ->(month) { where("DATE_PART('month',  updated_at) = ? AND DATE_PART('year',  updated_at) = ? AND status_id = ?", month,  Date.today.year, Status.find_by_name("Completed").id) }
+ scope :location, ->(location){ where("location_id = ?", Location.find_by_name(location).id)}
+ scope :category, ->(category){ where("category_id = ?", Category.find_by_name(category).id)}
 
    belongs_to :location
    belongs_to :category
