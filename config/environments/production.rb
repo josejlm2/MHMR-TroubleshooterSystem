4634a0f724b0f9956522b70f12b10bcec2c19169
Travis Olbrich <travis.olbrich@gmail.com>
2015-05-01 01:12:41 -0500

    Fixed syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ MhmrTroubleshooter::Application.configure do
     :user_name            => ENV['gmail_username'],
     :password             => ENV['gmail_password'],
     :authentication       => "plain",
-    :enable_starttls_auto => true
+    :enable_starttls_auto => true,
     :openssl_verify_mode  => 'none'
   }
   config.action_mailer.perform_deliveries = true
