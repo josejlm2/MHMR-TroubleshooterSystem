515766583008a35f565ea9700df0aaf014d98709
ruggiero3x <ruggiero3x@gmail.com>
2015-05-01 17:06:27 -0500

    restricted permissions of regular users

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,39 +1,79 @@
 class CategoriesController < ApplicationController
   def index
+    if current_user.admin?
 	@categories = Category.all
+    else
+      flash[:notice] = "Permission Denied!"
+      redirect_to root_path
+    end
   end
   def show
+    if current_user.admin?
 	@category = Category.find(params[:id])
+    else
+      flash[:notice] = "Permission Denied!"
+      redirect_to root_path
+    end
   end
 
   def new
+    if current_user.admin?
 	@category =Category.new
+    else
+      flash[:notice] = "Permission Denied!"
+      redirect_to root_path
+    end
   end
   def create
+    if current_user.admin?
 	@category = Category.new(params[:category])
 	if @category.save
 		redirect_to(:action => 'index')
 	else
 		render('new')
 	end
+    else
+      flash[:notice] = "Permission Denied!"
+      redirect_to root_path
+    end
   end
   def edit
+    if current_user.admin?
 	@category = Category.find(params[:id])
+    else
+      flash[:notice] = "Permission Denied!"
+      redirect_to root_path
+    end
   end
   def update
+    if current_user.admin?
 	@category = Category.find(params[:id])
 	if @category.update_attributes(params[:category])
 		redirect_to categories_path(@category)
 	else
 		render('index')
 	end
+    else
+      flash[:notice] = "Permission Denied!"
+      redirect_to root_path
+    end
   end
   def delete
+    if current_user.admin?
 	@category = Category.find(params[:id])
+    else
+      flash[:notice] = "Permission Denied!"
+      redirect_to root_path
+    end
   end
   def destroy
+    if current_user.admin?
   	@category = Category.find(params[:id])
 	@category.destroy
 	redirect_to categories_path
+    else
+      flash[:notice] = "Permission Denied!"
+      redirect_to root_path
+    end
   end
 end
