d334f1732164b7f4d3f190964341ca74811190f0
renzigalenzi <delliott002@satx.rr.com>
2015-05-05 22:18:44 -0500

    fixed user delete cucumber

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe TicketsController do
    it "responds sucessfully with an Http 200 status code" do
 	user = User.create!(email: 'name@test.com', password: 'password1', password_confirmation: 'password1', admin: true, manager: true)
-	allow(request.env['warden']).to receive(:authenticate!) {user}
-	allow(controller).to receive(:current_user) {user}
+		allow(request.env['warden']).to receive(:authenticate!) {user}
+		allow(controller).to receive(:current_user) {user}
 	get :index
 	expect(response).to be_success
    end
@@ -20,7 +20,7 @@ describe TicketsController do
       user = User.create!(email: 'name@test.com', password: 'password1', password_confirmation: 'password1', admin: true, manager: true)
       allow(request.env['warden']).to receive(:authenticate!) {user}
       allow(controller).to receive(:current_user) {user}
-      ticket1, ticket2 = Ticket.create!, Ticket.create!
+      ticket1, ticket2 = Ticket.create!(requestor: 'John Doe', phone: '555-478-9332', ru: 2,email: 'john@doe.com', room: 652, subject: 'Please Help', description: 'I have a problem', location_id: 1, category_id: 1, status_id: 1), Ticket.create!(requestor: 'John Doe', phone: '555-478-9332', ru: 2,email: 'john@doe.com', room: 652, subject: 'Please Help', description: 'I have a problem', location_id: 1, category_id: 1, status_id: 1)
       get :index
 
       expect(assigns(:tickets)).to match_array([ticket1, ticket2])
