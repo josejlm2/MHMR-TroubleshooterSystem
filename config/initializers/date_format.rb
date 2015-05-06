119dd1b2f02be855ee5ae1c3d0cdf8c9046e3ee6
Jose Manriquez <josejlm2@hotmail.com>
2015-05-05 20:59:16 -0500

    format time

---
+++ config/initializers/date_format.rb
@@ -0,0 +1,7 @@
+# Date
+# ----------------------------
+Date::DATE_FORMATS[:default] = "%e/%B/%Y" 
+
+# DateTime / Time
+# ----------------------------
+Time::DATE_FORMATS[:default] = "%m/%d/%Y %I:%M %p" 
